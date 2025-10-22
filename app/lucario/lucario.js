65953b604ac6b61e357c3756e58cac9777a6237f
the-coolest-wolf <hunterhignight.tx@gmail.com>
2025-10-22 11:45:59 -1000

    began styling and setting up the lucarion page

--- app/lucario/lucario.js
+++ app/lucario/lucario.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import LinkButton from '../components/LinkButton';
 
 export default function App() {
   return (
-    <View>
-      
+    <View style={styles.container}>
+      <View style={styles.picture}>
+        <Image source={{
+            uri : "https://img.pokemondb.net/artwork/large/lucario.jpg",
+          }}
+          style={{width:410, height: 165, borderColor: 'black', borderWidth: 2}}
+        />
+      </View>
+      <View>
+        <Text style={styles.title}>Lucario</Text>
+      </View>
     </View>
   );
 }
@@ -18,5 +27,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
+  },
+  picture: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    flex: 1,
+    fontSize: 100,
+  },
+  bodyText: {
+    flex:1,
+    fontSize: 40,
   }
 });
