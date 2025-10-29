a136d03a35a5266bd77e70ce93112bcf32b98321
the-coolest-wolf <hunterhignight.tx@gmail.com>
2025-10-29 12:01:26 -1000

    maybe finished lycanroc?

---
+++ app/Lycanroc/Rockruff.js
@@ -0,0 +1,16 @@
+import PokemonTemplate from '../../components/PokemonTemplate';
+
+export default function Charizard() {
+  const data = {
+    name: 'Rockruff',
+    type1: 'Rock',
+    type2: null,
+    shinyImage: { uri: 'https://pbs.twimg.com/media/FSyn7ELWYAUUOfA.jpg:largehttps://dinopixel.com/preload/1022/Shiny-Riolu.png' },
+    evolutionMethod: 'base pokemon form',
+    funFact: 'Rockruff has the ability to use the scent of its trainer to track them for days if it gets lost',
+    evolutions: [
+      { name: 'Rockruff', link: '/Lycanroc/Rockruff', image: 'https://pbs.twimg.com/media/FSyn7ELWYAUUOfA.jpg:large', type: 'Rock' },
+      { name: 'Lycanroc', link: '/Lycanric/Lycanroc', image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a25bad2e-b103-45c6-96b5-7a2e2e2b709d/dbxriou-95345834-1695-4f8f-b1b9-52faaf247f51.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi9hMjViYWQyZS1iMTAzLTQ1YzYtOTZiNS03YTJlMmUyYjcwOWQvZGJ4cmlvdS05NTM0NTgzNC0xNjk1LTRmOGYtYjFiOS01MmZhYWYyNDdmNTEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.nZEv4PQ9Cju0YAwHDpTmxvwi2Qz2aBRBnLTTtMnAfKg', type: 'Rock' },]
+  };
+  return <PokemonTemplate route={{ params: data }} />;
+}
