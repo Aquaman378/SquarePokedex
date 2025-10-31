b143415b5e910a16e28a31078ce749b702ec0907
Aquaman378 <jaonalexh37@yahoo.com>
2025-10-31 10:33:47 -1000

    Update shiny images and evolution links for Lucario, Mega Lucario, and Riolu

--- app/Lucario/Riolu.js
+++ app/Lucario/Riolu.js
@@ -5,13 +5,13 @@ export default function Charizard() {
     name: 'Riolu',
     type1: 'Fighting',
     type2: null,
-    shinyImage: { uri: 'https://dinopixel.com/preload/1022/Shiny-Riolu.png' },
+    shinyImage: { uri: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/447.png' },
     evolutionMethod: 'base pokemon form',
     funFact: 'Lucario can sense auras to understand and predict the emotions and movements of living beings from over half a mile away',
     evolutions: [
-      { name: 'Riolu', link: '/Lucario/Riolu', image: 'https://static.wikia.nocookie.net/pokemonempyrean/images/6/66/447.png/revision/latest?cb=20200825125040', type: 'Fighting' },
-      { name: 'Lucario', link: '/Lucario/Lucario', image: 'https://art.pixilart.com/68404e28f32dbed.png', type: 'Fighting' },
-      { name: 'Mega Lucario', link: '/Lucario/MegaLucario', image: 'https://www.nicepng.com/png/full/39-397478_mega-lucario-pixel-art-pokemon-mega-lucario.png', type: 'Fighting' },
+      { name: 'Riolu', link: '/Lucario/Riolu', image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/447.png', type: 'Fighting' },
+      { name: 'Lucario', link: '/Lucario/Lucario', image: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/448.png', type: 'Fighting' },
+      { name: 'Mega Lucario', link: '/Lucario/MegaLucario', image: 'https://sg.portal-pokemon.com/play/resources/pokedex/img/pm/600318f01f6fb95bbdaaebfd21644d1caefd43a7.png', type: 'Fighting' },
     ],
   };
   return <PokemonTemplate route={{ params: data }} />;
